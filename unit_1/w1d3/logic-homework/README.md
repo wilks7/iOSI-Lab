22f515790a625a93d3f5b26f10f77392e787d3ad
wilks7 <mikebw7@gmail.com>
2016-10-25 16:55:22 -0400

    w1d3 homework/lab

---
+++ unit_1/w1d3/logic-homework/README.md
@@ -0,0 +1,42 @@
+---
+title: Loops & Logic
+type: Homework
+duration: "1:00"
+creator:
+    name: Rudd Taylor
+    city: NYC
+---
+
+# ![](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png) Loops & Conditionals
+
+> ***Note:*** _This homework should be done independently._
+
+## Exercise
+
+ By the end of this exercise, you should use loops to avoid copying and pasting code when performing the same thing multiple times. You should also understand how to iterate over collections of items.
+
+#### Requirements
+
+- Write a for loop that capitalizes a string by iterating over its characters.
+- Write a while loop that does the same thing as the above.
+
+**Bonus:**
+- Print out all two-digit combinations of 0-9 (e.g. 00, 01, 02... 10, 11, 12.. 90, 91, 92... 99). Repeats are allowed, use a for loop within a for loop.
+
+#### Starter code
+
+Here is the [starter code for this assignment](https://github.com/generalassembly-studio/iOSI-course-materials/tree/master/curriculum/04-schedule-and-supplies/week-01/baseline-materials/logic-homework/starter-code).
+
+To get the assignment, you will need to fork the iOSI-course-materials repository and download it. Do your work on your own fork, commit and push it, then create a pull request using the Github website for your final submission back to the original iOSI-course-materials repo.
+
+> Note: Here is the [solution code](https://github.com/generalassembly-studio/iOSI-course-materials/tree/master/curriculum/04-schedule-and-supplies/week-01/baseline-materials/logic-homework/solution-code).
+
+#### Deliverable
+
+At the bottom of the file are unit tests. For your homework to succeed, all tests must pass.
+
+#### Assessment
+
+Open the playground file and follow instructions. The file includes a unit test that will automatically check to see if you got the right answers! Having trouble? Try revising your code. Still can't figure it out? Talk with your instructor and review the solution code.
+
+> Instructors: The playground file includes a unit test that will automatically check student input. If you want to further confirm their progress before their final submission, you could ask them to submit a PR with their modified playground file.
---
+++ unit_1/w1d3/logic-homework/starter-code/Homework.playground/Contents.swift
@@ -0,0 +1,78 @@
+//: Playground - noun: a place where people can play
+
+import XCTest
+
+// This homework involves two bits of new syntax: using a for loop to iterate over a 'collection', and getting the next index from an existing index
+
+// Here's a collection of integers
+let zeroToTen = 0...10 // This is a collection of all the integers from 0 to 10, inclusive
+// Let's say we want to do something with each of these variables. Easy! Just use the 'for' loop
+for number in zeroToTen {
+    print(number)
+} // This loop prints the numbers 0 through 10
+
+for number in 0...10 {
+    print(number)
+} // This loop is the same as the above, we just defined the collection inline with the for loop
+
+for character in "hi!".characters {
+    print(character)
+} // Here we're doing the same thing with characters
+
+// Here's how we get a count from a collection
+let numberCount = zeroToTen.count
+print(numberCount) // Prints 11
+
+// That's how we iterate over an entire collection. What if we just want one variable from a collection at a time? For now, we'll use this syntax:
+let string = "Hey!"
+let firstCharacterIndex = string.characters.startIndex
+let lastCharacterIndex = string.characters.endIndex
+print(string.characters[firstCharacterIndex])
+
+let secondCharacterIndex = string.index(after: firstCharacterIndex)
+print(string.characters[secondCharacterIndex])
+
+let thirdCharacterIndex = string.index(after: secondCharacterIndex)
+print(string.characters[thirdCharacterIndex])
+
+// Here's how we uppercase a character
+let firstCharacter = string.characters[firstCharacterIndex]
+let uppercaseFirstCharacter = String(firstCharacter).uppercased()
+
+// TODO: Write a for loop that capitalizes the above string by iterating over its characters.
+var lowercaseString = "i love general assembly!!"
+var uppercaseString = ""
+
+// TODO: Write a while loop that does the same thing as the above. First capture the startIndex of lowercaseString, then use index.successor() to get the next one
+var uppercaseWhileString = ""
+var index = lowercaseString.characters.startIndex
+
+print(uppercaseWhileString)
+
+
+// Bonus TODO: Print out all two-digit combinations of 0-9 (e.g. 00, 01, 02... 10, 11, 12.. 90, 91, 92... 99). Repeats are allowed, use a for loop within a for loop. numbersString should take the format: 000102030405060708091011121314... See the unit test below for the expected result.
+var numbersString = ""
+
+print(numbersString)
+
+class MyTests : XCTestCase {
+    func tests() {
+//                XCTAssertEqual(uppercaseString, "I LOVE GENERAL ASSEMBLY!!")
+//                XCTAssertEqual(uppercaseWhileString, "I LOVE GENERAL ASSEMBLY!!")
+        // Bonus:
+//                XCTAssertEqual(numbersString, "00010203040506070809101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899")
+    }
+}
+
+struct TestRunner {
+    func runTests(testClass: AnyClass) {
+        let tests = testClass as! XCTestCase.Type
+        let testSuite = tests.defaultTestSuite()
+        testSuite.tests.forEach { $0.run() }
+        let run = testSuite.testRun as! XCTestSuiteRun
+        
+        print("\(run.totalFailureCount) failures")
+    }
+}
+
+TestRunner().runTests(testClass: MyTests.self)
